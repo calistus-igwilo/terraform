9ace9dfbe82bd47ca69ab062149650d449df5181
calistus-igwilo <ebunilo@yahoo.com>
2022-05-28 16:29:04 +0100

    privte subnets

--- vpc_and_ec2/infrastructure/variables.tf
+++ vpc_and_ec2/infrastructure/variables.tf
@@ -22,3 +22,18 @@ variable "public_subnet_3_cidr" {
     description = "Public Subnet 3 CIDR"
   
 }
+
+variable "private_subnet_1_cidr" {
+    description = "Private Subnet 1 CIDR"
+  
+}
+
+variable "private_subnet_2_cidr" {
+    description = "Private Subnet 2 CIDR"
+  
+}
+
+variable "private_subnet_3_cidr" {
+    description = "Private Subnet 3 CIDR"
+  
+}
--- vpc_and_ec2/infrastructure/vpc.tf
+++ vpc_and_ec2/infrastructure/vpc.tf
@@ -47,3 +47,36 @@ resource "aws_subnet" "public-subnet-3" {
     }
   
 }
+
+resource "aws_subnet" "private-subnet-1" {
+    cidr_block = "${var.private_subnet_1_cidr}"
+    vpc_id = "${aws_vpc.production-vpc.id}"
+    availability_zone = "us-east-1a"
+
+    tags = {
+      "Name" = "Private-Subnet-1"
+    }
+  
+}
+
+resource "aws_subnet" "private_subnet_2_cidr" {
+    cidr_block = "${var.private_subnet_2_cidr}"
+    vpc_id = "${aws_vpc.production-vpc.id}"
+    availability_zone = "us-east-1b"
+
+    tags = {
+      "Name" = "Private-Subnet-2"
+    }
+  
+}
+
+resource "aws_subnet" "private_subnet_3_cidr" {
+    cidr_block = "${var.private_subnet_3_cidr}"
+    vpc_id = "${aws_vpc.production-vpc.id}"
+    availability_zone = "us-east-1c"
+
+    tags = {
+      "Name" = "Private-Subnet-3"
+    }
+  
+}
