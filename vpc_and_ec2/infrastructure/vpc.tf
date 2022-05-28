56398552beeb477059b2c3b0a6524f4c7f59c390
calistus-igwilo <ebunilo@yahoo.com>
2022-05-28 18:31:24 +0100

    routing

--- vpc_and_ec2/infrastructure/vpc.tf
+++ vpc_and_ec2/infrastructure/vpc.tf
@@ -95,3 +95,67 @@ resource "aws_route_table" "private-route-table" {
   }
   
 }
+
+resource "aws_route_table_association" "public-subnet-1-association" {
+  route_table_id = "${aws_route_table.public-route-table}"
+  subnet_id = "${aws_subnet.public-subnet-1.id}"
+  
+}
+
+resource "aws_route_table_association" "public-subnet-2-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id = "${aws_subnet.public-subnet-2.id}"
+  
+}
+
+resource "aws_route_table_association" "public-subnet-3-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id = "${aws_subnet.public-subnet-3.id}"
+  
+}
+
+resource "aws_route_table_association" "private-subnet-1-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id = "${aws_subnet.private-subnet-1.id}"
+  
+}
+
+resource "aws_route_table_association" "private-subnet-2-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id = "${aws_subnet.private-subnet-2.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-3-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id = "${aws_subnet.private-subnet-3.id}"
+  
+}
+
+resource "aws_eip" "elastic-ip-for-nat-gw" {
+  vpc = true
+  associate_with_private_ip = "10.0.0.5"
+
+  tags = {
+    "Name" = "Production-EIP"
+  }
+  
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
+  subnet_id = "${aws_subnet.public-subnet-1.id}"
+
+  tags = {
+    "Name" = "Production-NAT-Gw"
+  }
+  depends_on = [
+    "aws_eip.elastic-ip-for-nat-gw"
+  ]
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  nat_gateway_id = "${aws_nat_gateway.nat-gw.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  
+}
