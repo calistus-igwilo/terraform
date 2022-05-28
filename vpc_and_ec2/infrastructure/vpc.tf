4da9962ce49e68b4b33f0bdf279df83052905db8
calistus-igwilo <ebunilo@yahoo.com>
2022-05-28 18:58:00 +0100

    test run

--- vpc_and_ec2/infrastructure/vpc.tf
+++ vpc_and_ec2/infrastructure/vpc.tf
@@ -159,3 +159,19 @@ resource "aws_route" "nat-gw-route" {
   destination_cidr_block = "0.0.0.0/0"
   
 }
+
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = "${aws_vpc.production-vpc.id}"
+
+  tags = {
+    "Name" = "Production-IGw"
+  }
+  
+}
+
+resource "aws_route" "public-internet-gw-route" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  gateway_id = "${aws_internet_gateway.production-igw.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  
+}
